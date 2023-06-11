9c89c4b28e4ed5e3263670425d7308eef7343148
Bilal <mansuribilal101@gmail.com>
2023-06-12 00:35:45 +0530

    mogno connect fix

--- src/lib/mongoConnect.ts
+++ src/lib/mongoConnect.ts
@@ -4,11 +4,12 @@ export const connectMongoDB = async () => {
     if (mongoose.connection.readyState === 1) {
         return mongoose.connection.asPromise();
     }
-    if (process.env.MONGODB_URI) {
-        return mongoose.connect(process.env.MONGODB_URI, {
+
+    return mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfip8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+        {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        } as ConnectOptions);
-    }
-
+        } as ConnectOptions
+    );
 };
