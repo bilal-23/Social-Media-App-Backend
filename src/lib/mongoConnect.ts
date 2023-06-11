54b6ad3a018b7046288f1400d88cf5b0ec28a64c
Bilal <mansuribilal101@gmail.com>
2023-06-12 00:18:06 +0530

    :ZAP: Files Added

---
+++ src/lib/mongoConnect.ts
@@ -0,0 +1,14 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+export const connectMongoDB = async () => {
+    if (mongoose.connection.readyState === 1) {
+        return mongoose.connection.asPromise();
+    }
+    return mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfip8.mongodb.net/emojica?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        } as ConnectOptions
+    );
+};
