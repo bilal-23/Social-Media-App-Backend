e368c97b8424d9e747a8a3e15e8cb49b503f759d
Bilal <mansuribilal101@gmail.com>
2023-06-16 00:03:40 +0530

    :zap: MongoDB URI Changed

--- src/lib/mongoConnect.ts
+++ src/lib/mongoConnect.ts
@@ -4,9 +4,9 @@ export const connectMongoDB = async () => {
     if (mongoose.connection.readyState === 1) {
         return mongoose.connection.asPromise();
     }
-
+    const link = process.env.MONGODB_URI as string;
     return mongoose.connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfip8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+        link,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
