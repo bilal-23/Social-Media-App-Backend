68478a5b5357b3a0c9bdd986701055b4634daac7
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-06-20 15:30:38 +0530

    :sparkles: feat: added imgURL in post

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,24 +1,25 @@
-import mongoose, { Types } from 'mongoose';
-import { commentSchema } from './comment';
+import mongoose, { Types } from "mongoose";
+import { commentSchema } from "./comment";
 
 const postSchema = new mongoose.Schema({
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, //contains user Id of user who posted this post
-    content: { type: String, required: true },
-    likes: {
-        likeCount: { type: Number, default: 0 },
-        likedBy: {
-            type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-            default: []
-        }, //contains user Ids of users who liked this post
-        dislikedBy: {
-            type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-            default: []
-        }, //contains user Ids of users who disliked this post
-    },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, //contains user Id of user who posted this post
+  content: { type: String, required: true },
+  likes: {
+    likeCount: { type: Number, default: 0 },
+    likedBy: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      default: [],
+    }, //contains user Ids of users who liked this post
+    dislikedBy: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      default: [],
+    }, //contains user Ids of users who disliked this post
+  },
 
-    comments: { type: [commentSchema], default: [] }, //contains user Ids of users who commented on this post, along with their comments and timestamps
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+  comments: { type: [commentSchema], default: [] }, //contains user Ids of users who commented on this post, along with their comments and timestamps
+  imgURL: { type: String, default: "" }, // Added imgURL field: gives imgURL from data
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
-export const Post = mongoose.models.Post || mongoose.model('Post', postSchema);
+export const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
