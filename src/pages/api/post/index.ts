42b70c92983273d5a3023028c358ac9b36454548
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-06-24 01:29:03 +0530

    :bug: fix: lastName

--- src/pages/api/post/index.ts
+++ src/pages/api/post/index.ts
@@ -66,8 +66,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 async function getPosts() {
   const posts = await Post.find({})
     .populate([
-      { path: "author", select: "_id firstName pic username" },
-      { path: "comments.user", select: "_id firstName pic username" },
+      { path: "author", select: "_id firstName pic lastName username" },
+      { path: "comments.user", select: "_id firstName pic lastName username" },
     ])
     .sort({ createdAt: -1 }); // -1: DESC, 1: ASC
   return posts;
