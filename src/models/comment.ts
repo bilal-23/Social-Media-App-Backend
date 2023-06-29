1b2598e5c1829671435db76f695c8e7ac1f0f66f
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-06-29 23:34:21 +0530

    Update comment.ts

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+// test
 export const commentSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     comment: String,
